063dcda0f75941d27ca51f2bf62fe6feae6a31a0
bleak <ffeeaarrofdeath@mail.ru>
2023-11-13 04:14:45 +0300

    1h

---
+++ app/dashboard/products/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductsPage = () => {
+  return (
+    <div>tt</div>
+  )
+}
+
+export default ProductsPage
---
+++ app/login/page.jsx
@@ -0,0 +1,7 @@
+const LoginPage = () => {
+  return (
+    <div>sss</div>
+  )
+}
+
+export default LoginPage
